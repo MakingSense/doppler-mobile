8e366424105ceff1a37c888e9359e7e154eb0324
Unknown <lpelizza@makingsense.com>
2018-04-23 12:57:51 -0300

    Add campaign detail section

---
+++ Doppler.Mobile/Doppler.Mobile/Doppler.Mobile/ViewModels/CampaignReceipientsInfoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Doppler.Mobile.Core.Models;
+
+namespace Doppler.Mobile.ViewModels
+{
+    public class CampaignReceipientsInfoViewModel : BaseViewModel
+    {
+        public CampaignReceipientsInfoViewModel()
+        {
+            ListOfSubscribers = new ObservableCollection<SubscriberList>();
+            var listMock = CreateListOfSubscribersMock();
+            listMock.ToList().ForEach(ListOfSubscribers.Add);
+        }
+
+        public ObservableCollection<SubscriberList> ListOfSubscribers { get; set; }
+
+        private List<SubscriberList> CreateListOfSubscribersMock()
+        {
+            var list1 = new SubscriberList
+            {
+                Name = "Graphic Designers",
+                NumberOfSubscribers = 230
+            };
+
+            var list2 = new SubscriberList
+            {
+                Name = "Doctors",
+                NumberOfSubscribers = 5530
+            };
+
+            return new List<SubscriberList> { list1, list2 };
+        }
+    }
+}
