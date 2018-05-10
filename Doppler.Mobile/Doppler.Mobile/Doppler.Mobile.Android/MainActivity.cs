ebe4929d08a6ea62d0c6e2c1a34b9706ab022238
lpelizzaMS <lpelizza@makingsense.com>
2018-05-10 09:23:00 -0300

    Change apps configuration

--- Doppler.Mobile/Doppler.Mobile/Doppler.Mobile.Android/MainActivity.cs
+++ Doppler.Mobile/Doppler.Mobile/Doppler.Mobile.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using SegmentedControl.FormsPlugin.Android;
 
 namespace Doppler.Mobile.Droid
 {
-    [Activity(Label = "Doppler.Mobile", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Doppler", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
